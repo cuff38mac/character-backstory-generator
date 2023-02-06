93727ce973e61db676715e191e8b76e867300e42
cuff38mac <cuff38mac@yahoo.com>
2023-02-06 08:34:01 -0500

    Add files via upload

--- rpg-character-story/src/main/java/com/projects/rpgcharacterstory/repo/CharacterRepo.java
+++ rpg-character-story/src/main/java/com/projects/rpgcharacterstory/repo/CharacterRepo.java
@@ -4,6 +4,10 @@ import com.projects.rpgcharacterstory.model.Characters;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
+/**
+ * This repository configures the use of CRUD operations
+ */
 public interface CharacterRepo extends JpaRepository<Characters, Long> {
 }
