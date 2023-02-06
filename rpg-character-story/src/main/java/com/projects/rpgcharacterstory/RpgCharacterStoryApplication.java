6d65606de0a56d4a1ebbd05cff317654050551ba
cuff38mac <cuff38mac@yahoo.com>
2023-02-06 08:32:05 -0500

    Add files via upload

--- rpg-character-story/src/main/java/com/projects/rpgcharacterstory/RpgCharacterStoryApplication.java
+++ rpg-character-story/src/main/java/com/projects/rpgcharacterstory/RpgCharacterStoryApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * This application allows a user to select a character from a dropdown list, view
+ * its predefined stats, and generate random backstories specific to the character selected.
+ *
+ */
 public class RpgCharacterStoryApplication {
 
 	public static void main(String[] args) {
