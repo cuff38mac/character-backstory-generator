c6a2a1b6e249695b7bae0792a0411fcbac5d6cc0
cuff38mac <cuff38mac@yahoo.com>
2023-02-06 08:32:49 -0500

    Add files via upload

--- rpg-character-story/src/main/java/com/projects/rpgcharacterstory/controller/CharacterController.java
+++ rpg-character-story/src/main/java/com/projects/rpgcharacterstory/controller/CharacterController.java
@@ -1,3 +1,4 @@
+
 package com.projects.rpgcharacterstory.controller;
 
 import com.projects.rpgcharacterstory.model.Characters;
@@ -8,24 +9,43 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ * A REST Controller that allows for the retrieval and saving of characters from the SQL Database
+ */
 @RestController
 @CrossOrigin
 public class CharacterController {
 @Autowired
     private CharacterRepo characterRepo;
 
+    /**
+     * POST mapping to create a character
+     * @param newCharacter
+     * @return
+     */
 
 @PostMapping("/character")
     Characters newCharacter(@RequestBody Characters newCharacter){
     return characterRepo.save(newCharacter);
 }
 
+    /**
+     * GET call to get all the characters
+      * @return
+     */
 @GetMapping("/listAll")
     List<Characters> getAllCharacters(){
 
     return characterRepo.findAll();
 }
 
+
+    /**
+     * GET call that will retrieve the specified character by its id
+      * @param id
+     * @return
+     */
 @GetMapping("/find/{id}")
 Optional<Characters> findById(@PathVariable Long id){
 
