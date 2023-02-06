87b196bede11468388d701ca3be84bf7e3d1f440
cuff38mac <cuff38mac@yahoo.com>
2023-02-06 08:33:28 -0500

    Add files via upload

--- rpg-character-story/src/main/java/com/projects/rpgcharacterstory/model/Characters.java
+++ rpg-character-story/src/main/java/com/projects/rpgcharacterstory/model/Characters.java
@@ -1,3 +1,4 @@
+//
 package com.projects.rpgcharacterstory.model;
 
 import jakarta.persistence.Entity;
@@ -9,13 +10,25 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+/**
+ * This entity class maps to the database table called Characters.
+ */
 public class Characters {
 
     @Id
     @GeneratedValue
+    /**
+     * Represents the ID column of the table
+     */
     private Long id;
 
+    /**
+     * Represents the ClassName column of the table
+     */
     private String className;
 
+    /**
+     * Represents the image column of the table for URL links
+     */
     private String image;
 }
